a40fa92dee0673924334f4306782ffc8d7e90f69
PatriotRossii <patriotrossii2019@mail.ru>
2020-12-31 19:24:02 +0500

    Implement PartialOrd for Literal

--- src/literal.rs
+++ src/literal.rs
@@ -1,4 +1,4 @@
-use std::fmt::{self, Display};
+use std::{cmp::Ordering, fmt::{self, Display}};
 
 #[derive(Debug, Clone, Copy)]
 pub enum Literal {
@@ -13,12 +13,68 @@ impl Display for Literal {
 			&Literal::Integer(i) => {
 				write!(f, "{}", i)
 			},
-			&Literal::Unknown => {
-				write!(f, "")
-			},
 			&Literal::String(s) => {
 				write!(f, "{}", s)
 			}
+			&Literal::Unknown => {
+				write!(f, "")
+			},
 		}
     }
+}
+
+impl PartialEq for Literal {
+	fn eq(&self, other: &Self) -> bool {
+		match self {
+			&Literal::Integer(i) => {
+				if let &Literal::Integer(j) = other {
+					i == j
+				} else {
+					false
+				}
+			},
+			&Literal::String(s) => {
+				if let &Literal::String(os) = other {
+					s == os
+				} else {
+					false
+				}
+			}
+			&Literal::Unknown => {
+				if let &Literal::Unknown = other {
+					true
+				} else {
+					false
+				}
+			}
+		}
+	}
+}
+
+impl PartialOrd for Literal {
+	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+		match self {
+			&Literal::Integer(i) => {
+				if let &Literal::Integer(j) = other {
+					i.partial_cmp(&j)
+				} else {
+					None
+				}
+			},
+			&Literal::String(s) => {
+				if let &Literal::String(os) = other {
+					s.partial_cmp(os)
+				} else {
+					None
+				}
+			}
+			&Literal::Unknown => {
+				if let &Literal::Unknown = other {
+					None
+				} else {
+					None
+				}
+			}
+		}
+	}
 }
