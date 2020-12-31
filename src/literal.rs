bb30f57b56b051537c21d5657c9e812dc74078a0
PatriotRossii <patriotrossii2019@mail.ru>
2020-12-31 18:23:31 +0500

    Add more types of literals, implement Display trait for they

--- src/literal.rs
+++ src/literal.rs
@@ -1,4 +1,24 @@
+use std::fmt::{self, Display};
+
 #[derive(Debug, Clone, Copy)]
 pub enum Literal {
 	Integer(i64),
+	String(&'static str),
+	Unknown,
+}
+
+impl Display for Literal {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		match self {
+			&Literal::Integer(i) => {
+				write!(f, "{}", i)
+			},
+			&Literal::Unknown => {
+				write!(f, "")
+			},
+			&Literal::String(s) => {
+				write!(f, "{}", s)
+			}
+		}
+    }
 }
