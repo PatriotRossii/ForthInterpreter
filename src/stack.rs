b4607fece6d24ec914fefbda91c778585bc59f17
PatriotRossii <patriotrossii2019@mail.ru>
2020-12-31 18:24:10 +0500

    Implement auxillary stack methods

--- src/stack.rs
+++ src/stack.rs
@@ -3,22 +3,38 @@ pub struct Stack<T> {
         stack: Vec<T>
 }
 
-impl<T> Stack<T> {
+impl<T: Copy> Stack<T> {
 	pub fn new() -> Self {
 		Self {
 			stack: Vec::new()
 		}
 	}
 
-        pub fn push(&mut self, value: T) {
-                self.stack.push(value);
-        }
+    pub fn push(&mut self, value: T) {
+		self.stack.push(value);
+    }
 
-        pub fn pop(&mut self) -> Option<T> {
-                self.stack.pop()
-        }
+    pub fn pop(&mut self) -> Option<T> {
+        self.stack.pop()
+    }
 
 	pub fn last(&self) -> Option<&T> {
 		self.stack.last()
+	}
+
+	pub fn length(&self) -> usize {
+		self.stack.len()
+	}
+
+	pub(crate) fn swap(&mut self, a: usize, b: usize) {
+		self.stack.swap(a, b)
+	}
+
+	pub(crate) fn get(&self, a: usize) -> T {
+		self.stack[a]
+	}
+
+	pub(crate) fn remove(&mut self, a: usize) -> T {
+		self.stack.remove(a)
 	}
 }
