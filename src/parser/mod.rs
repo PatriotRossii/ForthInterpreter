d9521046bf02f7b29d88b9af74de040e9ad5daeb
PatriotRossii <patriotrossii2019@mail.ru>
2021-01-06 16:54:54 +0500

    Fix expression definition, remove unecessary imports

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -6,12 +6,10 @@ pub struct ForthParser;
 
 #[cfg(test)]
 mod tests {
-    use super::ForthParser;
+    use super::*;        
 
     #[test]
-    fn test_parse_comment() {
-        use super::*;        
-        
+    fn test_parse_comment() {        
         ForthParser::parse(Rule::COMMENT, "(hi)").unwrap();
         ForthParser::parse(Rule::COMMENT, "(h\ni)").unwrap_err();
         ForthParser::parse(Rule::COMMENT, "(hi").unwrap_err();
@@ -19,8 +17,6 @@ mod tests {
 
     #[test]
     fn test_parse_integer() {
-        use super::*;
-
         ForthParser::parse(Rule::integer, "123").unwrap();
         ForthParser::parse(Rule::integer, "-123").unwrap();
 
@@ -30,8 +26,6 @@ mod tests {
 
     #[test]
     fn test_parse_string() {
-        use super::*;
-
         ForthParser::parse(Rule::string, r#""hello""#).unwrap();
         ForthParser::parse(Rule::string, r#""""#).unwrap();
 
@@ -41,8 +35,6 @@ mod tests {
 
     #[test]
     fn test_parse_word() {
-        use super::*;
-
         ForthParser::parse(Rule::ident, "hello").unwrap();
         ForthParser::parse(Rule::ident, "hello_world").unwrap();
         ForthParser::parse(Rule::ident, "second_stack_2").unwrap();
@@ -53,9 +45,8 @@ mod tests {
 
     #[test]
     fn test_parse_expression() {
-        use super::*;
-
         ForthParser::parse(Rule::expression, "1 2 3 * +").unwrap();
+
         ForthParser::parse(Rule::expression, "2 5 *").unwrap();
         ForthParser::parse(Rule::expression, "foo").unwrap();
         ForthParser::parse(Rule::expression, "1 2 3 dup").unwrap();
