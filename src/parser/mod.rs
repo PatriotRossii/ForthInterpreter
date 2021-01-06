69c3105742b6b3490ab5ea592516c4759b2d9d83
PatriotRossii <patriotrossii2019@mail.ru>
2021-01-06 16:43:40 +0500

    Fix native_ident definition, add tests for words and expressions

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -43,11 +43,23 @@ mod tests {
     fn test_parse_word() {
         use super::*;
 
-        ForthParser::parse(Rule::word, "hello").unwrap();
-        ForthParser::parse(Rule::word, "hello_world").unwrap();
-        ForthParser::parse(Rule::word, "second_stack_2").unwrap();
+        ForthParser::parse(Rule::ident, "hello").unwrap();
+        ForthParser::parse(Rule::ident, "hello_world").unwrap();
+        ForthParser::parse(Rule::ident, "second_stack_2").unwrap();
 
-        ForthParser::parse(Rule::word, "8hello").unwrap_err();
+        ForthParser::parse(Rule::user_ident, "8hello").unwrap_err();
+        ForthParser::parse(Rule::ident, "*").unwrap();
+    }
+
+    #[test]
+    fn test_parse_expression() {
+        use super::*;
+
+        ForthParser::parse(Rule::expression, "1 2 3 * +").unwrap();
+        ForthParser::parse(Rule::expression, "2 5 *").unwrap();
+        ForthParser::parse(Rule::expression, "foo").unwrap();
+        ForthParser::parse(Rule::expression, "1 2 3 dup").unwrap();
+
     }
 
 }
