d905175a4aa38f9e006f13121df1a74acb71e0b1
PatriotRossii <patriotrossii2019@mail.ru>
2021-01-10 10:12:30 -0500

    Impl Parser for Expression

--- src/entities/complex/expression.rs
+++ src/entities/complex/expression.rs
@@ -1,10 +1,43 @@
 use crate::entities::simple::{literal::Literal, ident::Ident};
+use crate::parser::Parser;
 
 pub enum ExpressionElement {
     Literal(Literal),
     Ident(Ident),
 }
 
+impl Parser for ExpressionElement {
+	fn parse(pair: pest::iterators::Pair<parser::Rule>) -> Self {
+		match pair.as_rule() {
+			Rule::literal => {
+				ExpressionElement::Literal(Literal::parse(pair))
+			},
+			Rule::ident => {
+				ExpressionElement::Ident(Ident::parse(pair))
+			},
+		}
+	}
+}
+
 pub struct Expression {
     elements: Vec<ExpressionElement>,
+}
+
+impl Parser for Expression {
+	fn parse(pair: pest::iterators::Pair<parser::Rule>) -> Self {
+		let elements: Vec<ExpressionElement> = vec![];
+		for inner_pair in pair.into_inner() {
+			match inner_pair.as_rule() {
+				Rule::literal => {
+					elements.push(ExpressionElement::Literal(Literal::parse(inner_pair)));
+				},
+				Rule::ident => {
+					elements.push(ExpressionElement::Ident(Ident::parse(inner_pair)));
+				}
+			}
+		}
+		Self {
+			elements
+		}
+	}
 }
