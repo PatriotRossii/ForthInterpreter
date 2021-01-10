a870a6fa39e30f1e6bcebd34a2a94ddf82964cb8
PatriotRossii <patriotrossii2019@mail.ru>
2021-01-10 10:23:20 -0500

    Impl Parser for Line

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -3,8 +3,24 @@ pub mod complex;
 
 use simple::literal::Literal;
 use complex::{definition::Definition, expression::Expression};
+use crate::parser::Parser;
 
 enum Line {
     Definition(Definition),
     Expression(Expression),
+}
+
+
+impl Parser for Line {
+	fn parse(pair: pest::iterators::Pair<parser::Rule>) -> Self {
+		let inner = pair.into_inner().nth(0).unwrap();
+		match inner.as_rule() {
+			Rule::definition => {
+				Line::Definition(Definition::parse(inner))
+			},
+			Rule::expression => {
+				Line::Expression(Expression::parse(inner))
+			},
+		}
+	}
 }
